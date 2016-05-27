da96975854df754e72ec25e36743da03e22ba99a
zpq <307154456@qq.com>
2016-05-27 17:15:47 +0800

    add sessionwq

--- chatRoom/backend/webSocket.go
+++ chatRoom/backend/webSocket.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"github.com/gorilla/sessions"
 	"html/template"
+	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 var store = sessions.NewCookieStore([]byte("mychat"))
@@ -13,16 +18,30 @@ var store = sessions.NewCookieStore([]byte("mychat"))
 func MyHttpWeb(handler http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Server", "Goginx")
-
-		session, _ := store.Get(r, "session-name")
-		session.Values["foo"] = "bar"
-		session.Values[42] = 43
+		session, _ := store.Get(r, "session-name3")
+		if session.IsNew {
+			session.Values["sid"] = generateSessionId()
+			//need login
+		}
 		session.Save(r, w)
-		fmt.Println(session)
-
+		// fmt.Println(session.IsNew, session.Name(), session.Values["sid"])
 		handler.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
+}
+
+func generateSessionId() string {
+	nano := time.Now().UnixNano()
+	rand.Seed(nano)
+	rndNum := rand.Int63()
+	sessionId := Md5(Md5(strconv.FormatInt(nano, 10)) + Md5(strconv.FormatInt(rndNum, 10)))
+	return sessionId
+}
+
+func Md5(text string) string {
+	hashMd5 := md5.New()
+	io.WriteString(hashMd5, text)
+	return fmt.Sprintf("%x", hashMd5.Sum(nil))
 }
 
 func myHandler(w http.ResponseWriter, r *http.Request) {
