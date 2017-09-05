13411ed2f37fadb167efb817a56f9030b392ac1e
zhaopanqi <zhaopanqi@china-qizhi.com>
2017-09-05 18:23:24 +0800

    update

--- magicFight/BattleControl.go
+++ magicFight/BattleControl.go
@@ -2,9 +2,53 @@ package main
 
 type BattleControl struct {
 	*Battle
+	Groups       map[*User]*Group
+	Phase        string       // 战斗的当前阶段： 未开始， 开始， 进行中， 结束
+	CurrentGroup *Group       // 当前的行动方
+	CurrentTurn  int          // 当前的回合数
+	CurrentActor *GameBattler // 当前的行动者
+	IsActorEnd   bool         // 某一具体的行动者的行动是否结束
+	IsGroupEnd   bool         // 某一方行动是否结束
+	IsTurnEnd    bool         // 某一回合是否结束，即双方各自执行了一次行动
 }
 
 // MakeOrder ...生成战斗顺序
 func (b *BattleControl) MakeOrder() {
+
+}
+
+func (b *BattleControl) ChangeCurrentGroup() {
+	for k, _ := range b.Groups {
+		if b.CurrentGroup != b.Groups[k] {
+			b.CurrentGroup = b.Groups[k]
+		}
+	}
+}
+
+func (b *BattleControl) ChangeCurrentActor() {
+
+}
+
+func (b *BattleControl) TurnAdd() {
+	b.CurrentTurn++
+}
+
+func (b *BattleControl) TurnBegin() {
+
+}
+
+func (b *BattleControl) TurnEnd() {
+
+}
+
+func (b *BattleControl) ProsessTurn() {
+	b.GroupTurn()
+}
+
+func (b *BattleControl) GroupTurn(cg *Group) {
+
+}
+
+func (b *BattleControl) ActorTurn(at *GameBattler) {
 
 }
--- magicFight/battle.go
+++ magicFight/battle.go
@@ -9,15 +9,10 @@ type Battle struct {
 	BattleCategory int // 战斗类型 pve or pvp
 	RaidID         int // 副本id
 	RaidCategory   int // 副本类型
-	IsGegin        bool
-	IsEnd          bool
+	IsBattleGegin  bool
+	IsBattleEnd    bool
 	Users          []*User
-	Groups         map[*User]*Group
-	CurrentGroup   *Group
-	CurrentTurn    int    // 当前的回合数
-	MaxTurn        int    // 战斗最大回合数
-	IsOneTurnEnd   int    // 某一回合是否结束，即双方各自执行了一次行动
-	Phase          string // 战斗的当前阶段： 未开始， 开始， 进行中， 结束
+	MaxTurn        int // 战斗最大回合数
 }
 
 func (b *Battle) SetUp() {
